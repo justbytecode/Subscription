614f23387f05545ac7c01b65eaa7bc0679fba7ee
Pawan Lode <pawanjlode@gmail.com>
2025-05-18 21:37:28 +0530

    fix: analytics for vercel

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ import Provider from "@/components/Provider";
 import Footer from "@/components/Footer";
 import { ToastContainer } from "react-toastify";
 import { ToastProvider } from "@/components/ui/toast";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default async function RootLayout({ children }) {
         <ToastContainer />
         {!session && <Navbar />}
         {children}
+        <Analytics/>
         {!session && <Footer />}
        </Provider>
        </ToastProvider>
