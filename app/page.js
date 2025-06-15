6906f79a1b98fb4e157b970a2c8cd58c962d5e92
Ekagra444 <ekagra444@gmail.com>
2025-06-15 20:59:13 +0530

    main image added in mobile view

--- app/page.js
+++ app/page.js
@@ -357,7 +357,32 @@ export default function Page() {
                   </Link>
                 </motion.div>
               </motion.div>
-
+              <motion.img
+          src="/new_background.png" 
+          className="  z-[-10] sm:hidden"
+          height={900}
+          width={900}
+          initial={{ 
+            opacity: 0, // Use a value between 0 (transparent) and 1 (opaque)
+            filter: "blur(10px) brightness(0.5)",
+            transform: "translateX(5px)"
+          }}
+          animate={{ 
+            opacity: 0.8, // Final opacity value
+            filter: "blur(0px) brightness(1)",
+            transform: "translateX(0px) scale(1)"
+          }}
+          transition={{
+            duration: 1.5,
+            delay: 0.3,
+            ease: "easeOut"
+          }}
+          whileHover={{
+            opacity: 1,
+            filter: "blur(0px) brightness(1.2)",
+            transition: { duration: 0.3 }
+          }}
+        />
             {/* from here only in desktop view */}
             {/* Feature icons in horizontal layout */}
             {/* large devices */}
